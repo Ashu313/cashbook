f37d1e8612d2b94d1881c3d011b3a1ba7a120053
Ashu313 <79858134+Ashu313@users.noreply.github.com>
2023-01-27 03:03:08 +0530

    navbar changes

--- src/client/container/Navbar/Navbar.js
+++ src/client/container/Navbar/Navbar.js
@@ -8,7 +8,8 @@ const Navbar=()=>{
   <>
         <div className='navbar-container'>
             <div className="menu-icons">
-                <img src='https://static.vecteezy.com/system/resources/previews/014/208/066/original/expense-ratio-3d-rendering-isometric-icon-png.png' alt="icons"></img>
+          {  /*    <img src='https://static.vecteezy.com/system/resources/previews/014/208/066/original/expense-ratio-3d-rendering-isometric-icon-png.png' alt="icons"></img>*/}
+          <span style={{'color':'orange'}}>Expense Tracker</span>
             </div>
             <div className='content1'>
                 <ul className='content-data'>
